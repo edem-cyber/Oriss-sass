5f7122637e33fb16a9485ebaf7189e7b33c42b9e
Edem Kojo Agbakpe <edem.agbakpe@gmail.com>
2023-11-12 22:20:42 +0000

    nxt seo out

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
 import { ClerkProvider } from '@clerk/nextjs';
-import { DefaultSeo } from 'next-seo';
+
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -19,25 +19,25 @@ export default function RootLayout({
 }) {
   return (
     <ClerkProvider>
-      <DefaultSeo
+      {/* <DefaultSeo
         title="Oriss | AI Powered Tools"
         description="Next SEO is a plug in that makes managing your SEO easier in Next.js projects."
 
-        openGraph={{
-          type: 'website',
-          title: 'Orissa | AI Powered Tools',
-          description: 'Oriss is a suite of tools used for your day to day designing, programming, writing & creating.',
-          locale: 'en_IE',
-          url: 'https://www.url.ie/',
-          siteName: 'Oriss',
-        }}
+        // openGraph={{
+        //   type: 'website',
+        //   title: 'Orissa | AI Powered Tools',
+        //   description: 'Oriss is a suite of tools used for your day to day designing, programming, writing & creating.',
+        //   locale: 'en_IE',
+        //   url: 'https://www.url.ie/',
+        //   siteName: 'Oriss',
+        // }}
       // twitter={{
       //   handle: '@handle',
       //   site: '@site',
       //   cardType: 'summary_large_image',
       // }}
 
-      />
+      /> */}
 
       <html lang="en">
         <body className={inter.className}>{children}</body>
