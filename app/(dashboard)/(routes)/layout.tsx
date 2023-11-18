c39c5192ec551f2a16caa12670bfb820e18373b3
Edem Kojo Agbakpe <edem.agbakpe@gmail.com>
2023-11-18 02:15:00 +0000

    loading added

--- app/(dashboard)/(routes)/layout.tsx
+++ app/(dashboard)/(routes)/layout.tsx
@@ -6,12 +6,12 @@ const DashboardLayout = ({
     return (
         <div className="h-full relative" >
             <div className="hidden h-full md:flex md:w-72 md:flex-col md:fixed md:inset-y-0 z-[80]bg - gray - 900">
-                <div >
-                    Hello Sidebar
-                </div >
+                <div>
+                    {/* Hello Sidebar */}
+                </div>
             </div >
             <main className="md:pl-72" >
-                Hello Content
+                {/* Hello Content */}
                 {children}
             </main >
         </div >
