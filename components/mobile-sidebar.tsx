b210ebdac03de93a2ff91e990605693f11df7720
Edem Kojo Agbakpe <edem.agbakpe@gmail.com>
2023-11-25 11:52:54 +0000

    fixed sidebar

--- components/mobile-sidebar.tsx
+++ components/mobile-sidebar.tsx
@@ -1,40 +1,32 @@
-// "use client"
+"use client";
 
-// import { Menu } from "lucide-react"
+import { useEffect, useState } from "react";
 
-// import { Button } from "@/components/ui/button"
-// // import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet"
-// import Sidebar from "@/components/sidebar"
-// import { useEffect, useState } from "react"
+import { Menu } from "lucide-react";
+import Sidebar from "@/components/sidebar";
+import { Button } from "@/components/ui/button";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 
 // interface MobileSidebarProps {
 //     apiLimitCount: number;
 //     isPro: boolean;
 // }
 
-// const MobileSidebar = ({apiLimitCount, isPro = false}:MobileSidebarProps) => {
-//     const [isMounted, setIsMounted] = useState(false);
+const MobileSidebar = (
+    // {apiLimitCount, isPro = false}:MobileSidebarProps
+) => {
+    return (
+        <Sheet >
+            <SheetTrigger >
+                <Button variant="ghost" size="icon" className="md:hidden" >
+                    <Menu />
+                </Button>
+            </SheetTrigger>
+            <SheetContent side="left" className="p-0" suppressHydrationWarning>
+                <Sidebar />
+            </SheetContent>
+        </Sheet>
+    );
+};
 
-//     useEffect(() => {
-//         setIsMounted(true)
-//     }, [])
-
-//     if(!isMounted) {
-//         return null;
-//     }
-    
-//     return (
-//         // <Sheet >
-//         //     <SheetTrigger >
-//         //         <Button variant="ghost" size="icon" className="md:hidden" >
-//         //             <Menu />
-//         //         </Button>
-//         //     </SheetTrigger>
-//         //     <SheetContent side="left" className="p-0">
-//         //      <Sidebar />
-//         //     </SheetContent>
-//         // </Sheet>
-//     )
-// }
-
-// export default MobileSidebar
+export default MobileSidebar;
--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -80,7 +80,7 @@ const Sidebar = ({
 
   const pathname = usePathname()
   return (
-    <div className="space-y-4 py-4 flex flex-col h-full  text-white">
+    <div className="space-y-4 py-4 flex flex-col h-full  text-white bg-[#203e25]">
       <div className="px-3 py-2 flex-1">
         <Link href="/dashboard" className="flex items-center pl-3 mb-14">
           <div className="relative w-8 h-8 mr-4">
