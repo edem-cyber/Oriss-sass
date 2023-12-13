fb246dde8c0488d8d8fb865ade7bbc7df2ea1a25
Edem Kojo Agbakpe <edem.agbakpe@gmail.com>
2023-12-13 05:59:30 +0000

    heading erro fix

--- app/(dashboard)/(routes)/settings/page.tsx
+++ app/(dashboard)/(routes)/settings/page.tsx
@@ -1,11 +1,10 @@
-import { Heading } from "@/components/Heading";
+import { Heading } from "@/components/heading";
 import { SubscriptionButton } from "@/components/subscription-button";
 import { checkSubscription } from "@/lib/subscription";
 import { Settings } from "lucide-react";
 
 const SettingPage = async () => {
-
-    const isPro = await checkSubscription()
+    const isPro = await checkSubscription();
     return (
         <div>
             <Heading
@@ -17,7 +16,9 @@ const SettingPage = async () => {
             />
             <div className="px-4 lg:px-8 space-y-4">
                 <div className="text-muted-foreground text-sm">
-                    {isPro ? "You are currently on a pro plan." : "You are currently on a free plan"}
+                    {isPro
+                        ? "You are currently on a pro plan."
+                        : "You are currently on a free plan"}
                 </div>
                 <SubscriptionButton isPro={isPro} />
             </div>
