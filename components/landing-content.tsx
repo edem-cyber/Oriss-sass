84c51b834b3bcff8a8f79a2f8f22874bf4160c9f
Edem Kojo Agbakpe <edem.agbakpe@gmail.com>
2023-12-25 10:09:36 +0000

    testimonials edit

--- components/landing-content.tsx
+++ components/landing-content.tsx
@@ -45,7 +45,7 @@ export const LandingContent = () => {
         Testimonials
       </h2>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-        {testimonials.map((item) => (
+        {/* {testimonials.map((item) => (
           <Card
             key={item.description}
             className="bg-[#192339] border-none text-white">
@@ -61,7 +61,7 @@ export const LandingContent = () => {
               </CardContent>
             </CardHeader>
           </Card>
-        ))}
+        ))} */}
       </div>
     </div>
   );
